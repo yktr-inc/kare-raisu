f259dd1b257dd442189abe67deab8338f02fb3b1
VARVEROPOULOS Valentin <valentin.varveropoulos@hotmail.fr>
2019-04-13 17:33:18 +0200

    user auth

---
+++ api/app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+    skip_before_action :authenticate_request, only: %i[login register]
+
+      def register
+        @user = User.create(user_params)
+       if @user.save
+        response = { message: 'User created successfully'}
+        render json: response, status: :created 
+       else
+        render json: @user.errors, status: :bad
+       end 
+      end
+  
+      def login
+        authenticate params[:email], params[:password]
+      end
+    
+      private
+    
+      def user_params
+        params.permit(
+          :name,
+          :email,
+          :password
+        )
+      end
+  
+      def authenticate(email, password)
+        command = AuthenticateUser.call(email, password)
+    
+        if command.success?
+          render json: {
+            access_token: command.result,
+            message: 'Login Successful'
+          }
+        else
+          render json: { error: command.errors }, status: :unauthorized
+        end
+       end  
+    end
