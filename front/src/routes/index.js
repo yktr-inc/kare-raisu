49a36c07878833cd466bf5fa14bd2ef9624db946
Louis Harang <vundaboy@gmail.com>
2019-04-19 19:13:53 +0200

    Bootstrap front

---
+++ front/src/routes/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Home from '@/views/Home.vue';
+import NotFound from '@/views/errors/NotFound.vue';
+
+import LayoutDefault from '@/layouts/LayoutDefault.vue';
+import LayoutModal from '@/layouts/LayoutModal.vue';
+
+import store from '@/store';
+
+Vue.use(Router);
+
+export const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      component: LayoutDefault,
+      path: '',
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: Home,
+          meta: {
+            auth: true
+          }
+        },
+      ]
+    },
+    {
+      path: '*',
+      component: NotFound
+    }
+  ]
+});
+
+export default router;
