8daa0fdfcfefa79cda00c2661601f674e12ae1a6
Audric <odrikm@hotmail.fr>
2019-05-15 10:46:54 +0200

    add inputs in profile view

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="feed" v-infinite-scroll="loadMore" infinite-scroll-disabled="isLoading" infinite-scroll-distance="10">
-    <p>Profile</p>
+    <p>Update your profile</p>
+    	<el-input placeholder="Firstname" v-model="input1"></el-input>
+    	<el-input placeholder="Lastname" v-model="input2"></el-input>
+    	<el-input placeholder="Email" v-model="input3"></el-input>
+
+    	<el-row>
+  <el-button>Save</el-button>
+  </el-row>
+
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      input1: '',
+      input2: '',
+      input3: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-input {
+  	margin-bottom: 15px;
+  }
+</style>
