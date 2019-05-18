c86ee0dce6bb8a1dc7757f8d5bbd6e8a15a7133c
Audric <odrikm@hotmail.fr>
2019-05-18 17:56:20 +0200

    add link to /post

--- front/src/components/Card.vue
+++ front/src/components/Card.vue
@@ -1,19 +1,21 @@
 <template>
-  <el-card class="feed-card">
-    <div slot="header" class="clearfix">
-      <span class="title">Lorem ipsum dolor sit amet</span>
-      <div><span class="credit">John Smith</span>
-      <span class="details"> · 1 hour ago · 3 comments</span></div>
-    </div>
-    <div class="text item">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-  </el-card>
+  <router-link to="/post">
+    <el-card class="feed-card">
+      <div slot="header" class="clearfix">
+        <span class="title">Lorem ipsum dolor sit amet</span>
+        <div><span class="credit">John Smith</span>
+        <span class="details"> · 1 hour ago · 3 comments</span></div>
+      </div>
+      <div class="text item">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </el-card>
+  </router-link>
 </template>
 
 <style scoped>
